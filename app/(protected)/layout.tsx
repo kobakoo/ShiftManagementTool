23831b077b059bd7a02db97ab71f08b1d7a18f7e
kobako <info@kobakoo.com>
2025-09-07 21:22:28 +0900

    second commit

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,10 +1,11 @@
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { ReactElement } from "react";
+import { ReactNode } from "react";
 import { Toaster } from "@/components/ui/sonner";
 import Footer from "@/components/Footer";
-const Layout = async ({ children }: { children: ReactElement }) => {
+
+const Layout = async ({ children }: { children: ReactNode }) => {
   const session = await getServerSession(authOptions);
 
   if (!session?.user) redirect(`/`);
--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,9 +1,9 @@
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { ReactElement } from "react";
+import { ReactElement, ReactNode } from "react";
 import { Toaster } from "@/components/ui/sonner";
-const Layout = async ({ children }: { children: ReactElement }) => {
+const Layout = async ({ children }: { children: ReactNode }) => {
   const session = await getServerSession(authOptions);
 
   if (session?.user) redirect(`/home`);
